7e608673491c2161e437c9ff9a3fb0d321aab59c
kartik19270-spec <kartik.19270@gmail.com>
2026-09-23 21:26:56 +0530

    Update hospital databse.sql

--- hospital databse.sql
+++ hospital databse.sql
@@ -1,1 +1,13 @@
-create database hospital_recommendation;
+#create database hospital_recommendation;
+use  hospital_recommendation;
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15),
+    email VARCHAR(150) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+select * from users;
